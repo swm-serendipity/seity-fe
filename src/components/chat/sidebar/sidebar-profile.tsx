bfb5d8d3432b7e9cb5264dd77702bec7848b68af
ShinJongung <whddnd0728@gmail.com>
2023-07-28 21:35:43 +0900

    🐛 fix: 로그인 버그 수정 (#8)
    #8

--- src/components/chat/sidebar/sidebar-profile.tsx
+++ src/components/chat/sidebar/sidebar-profile.tsx
@@ -7,14 +7,14 @@ export default function SideBarProfile() {
   return (
     <div className="flex gap-2.5 ml-5 mt-16">
       <div className="rounded-full bg-yellow-400 w-[44px] h-[44px] text-h3 flex justify-center items-center">
-        {isFetched && data.name ? data.name[0] : "A"}
+        {isFetched && data?.name ? data.name[0] : "A"}
       </div>
       <div>
         <p className="text-h4 text-blackbg-default">
-          {isFetched && data.name ? data.name : "사용자"}
+          {isFetched && data?.name ? data.name : "사용자"}
         </p>
         <p className="text-body-small text-blackbg-info">
-          {isFetched && data.email ? data.email : "email@mail.com"}
+          {isFetched && data?.email ? data.email : "email@mail.com"}
         </p>
       </div>
     </div>
