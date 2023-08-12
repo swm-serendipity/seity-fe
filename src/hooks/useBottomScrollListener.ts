d2299dc289b0197281a22527cacb26e5414113cf
ShinJongung <whddnd0728@gmail.com>
2023-08-12 17:01:16 +0900

    🐛 fix: 아무것도 없는 chat 화면에서 isBottom 활성화 되는 문제 해결 (#8)
    #8

--- src/hooks/useBottomScrollListener.ts
+++ src/hooks/useBottomScrollListener.ts
@@ -1,16 +1,20 @@
 import { RefObject, useEffect, useState } from "react";
 
 const useBottomScrollListener = (ref: RefObject<HTMLDivElement>) => {
-  const [isBottom, setIsBottom] = useState(false);
+  const [isBottom, setIsBottom] = useState(true);
 
   const handleScroll = () => {
     const { scrollHeight, scrollTop, clientHeight } = ref.current!;
-    const isAtBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    const isAtBottom =
+      clientHeight === scrollHeight ||
+      Math.ceil(scrollTop + clientHeight) >= scrollHeight;
     setIsBottom(isAtBottom);
   };
 
   useEffect(() => {
     if (!ref.current) return;
+    handleScroll();
     ref.current!.addEventListener("scroll", handleScroll);
     return () => {
       if (!ref.current) return;
