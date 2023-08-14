81e6d71de1a9596e2945ff366e02b19026a5e80f
ShinJongung <whddnd0728@gmail.com>
2023-08-14 14:36:51 +0900

    ♻️ refactor: meta data 추가 (#8)
    #8

--- src/app/(user)/posts/popular/page.tsx
+++ src/app/(user)/posts/popular/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+export const metadata: Metadata = {
+  title: "Seity | 인기 프롬프트",
+  description: "사내 인기 프롬프트",
+};
+
 import PostPopularBox from "@/components/user/posts/posts-popular/posts-popular-box";
+import { Metadata } from "next";
 
 export default function PopularPromptPage() {
   return <PostPopularBox />;
