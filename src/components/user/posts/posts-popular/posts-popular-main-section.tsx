6c49a15ea25ac71279821031f8caa9a26df5695d
ShinJongung <whddnd0728@gmail.com>
2023-08-15 12:22:31 +0900

    🐛 fix: 공유된 데이터 없을때 예외처리 추가 (#9)
    #9

--- src/components/user/posts/posts-popular/posts-popular-main-section.tsx
+++ src/components/user/posts/posts-popular/posts-popular-main-section.tsx
@@ -10,25 +10,31 @@ export default function PostsPopularMainSection({
   return (
     <div className="flex flex-col mt-14 w-full">
       <p className="font-h1 text-h3 px-12 mb-4">우리 부서 내 인기 프롬프트🔥</p>
-      <div
-        className="flex gap-5 flex-shrink-0 whitespace-nowrap overflow-x-auto custom-scrollbar
-      ml-12 pb-1 last:pr-6"
-      >
-        {posts.slice(0, 3).map((post) => {
-          return <PostsPopularMainCard post={post} key={post.id} />;
-        })}
 
-        <div className="flex flex-col justify-center items-center ml-7 gap-2">
-          <div
-            className="box-border w-11 h-11 border border-gray-300 
+      {posts.length > 0 ? (
+        <div
+          className="flex gap-5 flex-shrink-0 whitespace-nowrap overflow-x-auto custom-scrollbar
+         ml-12 pb-1 last:pr-6"
+        >
+          {posts.slice(0, 3).map((post) => {
+            return <PostsPopularMainCard post={post} key={post.id} />;
+          })}
+          <div className="flex flex-col justify-center items-center ml-7 gap-2">
+            <div
+              className="box-border w-11 h-11 border border-gray-300 
           rounded-full flex flex-col justify-center items-center relative"
-          >
-            <div className="absolute w-4 h-px border border-whitebg-info" />
-            <div className="absolute w-4 h-px border border-whitebg-info -rotate-90" />
+            >
+              <div className="absolute w-4 h-px border border-whitebg-info" />
+              <div className="absolute w-4 h-px border border-whitebg-info -rotate-90" />
+            </div>
+            <div className="text-body-medium text-whitebg-info">더보기</div>
           </div>
-          <div className="text-body-medium text-whitebg-info">더보기</div>
         </div>
-      </div>
+      ) : (
+        <div className="ml-12 min-w-[410px] h-[304px]">
+          등록된 게시글이 없습니다.
+        </div>
+      )}
     </div>
   );
 }
--- src/components/user/posts/posts-popular/posts-popular-sub-section.tsx
+++ src/components/user/posts/posts-popular/posts-popular-sub-section.tsx
@@ -17,26 +17,50 @@ export default function PostsPopularSubSection({
         <div className="flex-col flex">
           <p className="font-h1 text-h3 mb-4">최근 공유된 프롬프트✨</p>
           <div className="flex gap-2.5 flex-col overflow-y-auto custom-scrollbar pb-4">
-            {recentHotPosts.slice(0, 4).map((post) => (
-              <PostsPopularSubCard post={post} key={post.id} />
-            ))}
+            {recentHotPosts.length > 0 ? (
+              recentHotPosts
+                .slice(0, 4)
+                .map((post) => (
+                  <PostsPopularSubCard post={post} key={post.id} />
+                ))
+            ) : (
+              <div className="w-[410px] h-[125px] rounded-2xl">
+                등록된 게시글이 없습니다.
+              </div>
+            )}
           </div>
         </div>
 
         <div className="flex-col flex">
           <p className="font-h1 text-h3 mb-4">명예의 전당🥇</p>
           <div className="flex gap-2.5 flex-col overflow-y-auto custom-scrollbar pb-4">
-            {allHotPosts.slice(0, 4).map((post) => (
-              <PostsPopularSubCard post={post} key={post.id} />
-            ))}
+            {allHotPosts.length > 0 ? (
+              allHotPosts
+                .slice(0, 4)
+                .map((post) => (
+                  <PostsPopularSubCard post={post} key={post.id} />
+                ))
+            ) : (
+              <div className="w-[410px] h-[125px] rounded-2xl">
+                등록된 게시글이 없습니다.
+              </div>
+            )}
           </div>
         </div>
         <div className="flex-col flex mr-12">
           <p className="font-h1 text-h3 mb-4">이번 주 인기 프롬프트🥇</p>
           <div className="flex gap-2.5 flex-col overflow-y-auto custom-scrollbar pb-4">
-            {recentSharedPosts.slice(0, 4).map((post) => (
-              <PostsPopularSubCard post={post} key={post.id} />
-            ))}
+            {recentHotPosts.length > 0 ? (
+              recentSharedPosts
+                .slice(0, 4)
+                .map((post) => (
+                  <PostsPopularSubCard post={post} key={post.id} />
+                ))
+            ) : (
+              <div className="w-[410px] h-[125px] rounded-2xl">
+                등록된 게시글이 없습니다.
+              </div>
+            )}
           </div>
         </div>
       </div>
