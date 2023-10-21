78582ba13beda1f9da82283fcd63ad3b5e674399
ShinJongung <whddnd0728@gmail.com>
2023-10-21 21:33:36 +0900

    🐛 fix: lottie import 에러 수정 및 tailwind 디자인 수정 (#10)
    #10

--- src/components/ui/semi-color-button.tsx
+++ src/components/ui/semi-color-button.tsx
@@ -15,9 +15,9 @@ export default function SemiColorButton({
     <button
       disabled={disabled}
       className={`rounded-md text-center text-body-medium ${
-        disabled && "text-whitebg-disable bg-[#DBDBDB]"
-      } ${
-        type == "positive"
+        disabled
+          ? "text-whitebg-disable bg-[#DBDBDB]"
+          : type == "positive"
           ? "bg-[#D0FFF4] text-[#12A480]"
           : "bg-[#FFD9D9] text-[#FF3434]"
       }  w-[60px] h-9`}
